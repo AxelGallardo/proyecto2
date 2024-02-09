8de9f86a2fba49abfd51283f234fe9efd89679de
Axel <axel.gallardo.e@gmail.com>
2024-02-08 21:07:01 -0500

    hoy unifiqué el fondo morado con las modificaciones al header, chat y footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
 
 const styles = StyleSheet.create({
     footer: {
-        backgroundColor: 'blue',
+        backgroundColor: 'transparent',
         padding: 20,
         alignItems: 'center',
     },
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Switch } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Header = () => {
+    const [isEnabled, setIsEnabled] = useState(false);
+
+    const toggleSwitch = () => {
+        setIsEnabled(previousState => !previousState);
+    };
+
+    const handleSettingsPress = () => {
+        // Lógica para manejar el press del botón de configuración
+        console.log('Botón de configuración presionado');
+    };
+
     return (
         <View style={styles.header}>
-            <Text style={styles.headerText}>Olimpaaao</Text>
+            <View style={styles.olimpoContainer}>
+                <Text style={styles.headerText}>OlimpoChat</Text>
+            </View>
+            <View style={styles.switchContainer}>
+                <TouchableOpacity onPress={toggleSwitch} style={styles.switchButton}>
+                    <Text style={styles.switchText}>{isEnabled ? 'Visible' : 'Oculto'}</Text>
+                </TouchableOpacity>
+                <Switch
+                    trackColor={{ false: '#767577', true: '#FF00FF' }}
+                    thumbColor={isEnabled ? 'white' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                />
+            </View>
+
+            <TouchableOpacity onPress={handleSettingsPress} style={styles.settingsButton}>
+                <Icon name="settings" size={24} color="white" />
+            </TouchableOpacity>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     header: {
-        backgroundColor: 'blue',
-        padding: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
+        backgroundColor: 'transparent', // Cambiado el color de fondo
+        paddingVertical: 20, // Modificado para mayor espacio
+        paddingHorizontal: 15, // Modificado para mayor espacio
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    switchButton: {
+        paddingHorizontal: 10, // Añadido para espaciar el botón del switch
+    },
+    switchText: {
+        color: 'white',
+        marginRight: 5,
+        fontSize: 16, // Ajustado el tamaño de fuente
+    },
+    olimpoContainer: {
+        flex: 1, // Modificado para ocupar espacio restante
+        alignItems: 'flex-start', // Alineado hacia la izquierda
+        justifyContent: 'flex-start', // Alineado hacia la izquierda
     },
     headerText: {
         color: 'white',
-        fontSize: 20,
-
+        fontSize: 22,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    settingsButton: {
+        padding: 10,
     },
 });
 
 export default Header;
-
